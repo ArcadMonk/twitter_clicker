f5e1cf704f7fe108cd786f285a41ac45c7d78512
Baptiste <38102574+ArcadMonk@users.noreply.github.com>
2018-07-19 00:54:51 +0200

    code serveur fonctionnel (!mise en page)

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-var io = require('socket.io')(80);
-var cfg = require('./config.json');
-var tw = require('node-tweet-stream')(cfg);
-tw.track('socket.io');
-tw.track('javascript');
-tw.on('tweet', function(tweet){
-  io.emit('tweet', tweet);
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    fs = require('fs'),
+    sys = require('sys'),
+    twitter = require('twitter');
+
+app.listen(1337);
+
+var twit = new twitter({
+  consumer_key: 'CONSUMER_KEY',
+  consumer_secret: 'CONSUMER_SECRET',
+  access_token_key: 'ACCESS_TOKEN_KEY',
+  access_token_secret: 'ACCESS_TOKEN_SECRET'
+});
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+var twee = io.of('tweet');
+
+
+twit.stream('statuses/filter', { track: 'javascript' }, function(stream) {
+  stream.on('data', function (data) {
+    io.sockets.emit('tweet', data.text);
+    console.log('.');
+  });
 });
